ffe953ee25381c2dd70cf1ae6ee0fb48402ade7a
shruthij2703-code <shruthij.2703@gmail.com>
2026-02-06 06:44:08 +0530

    Delete Computer vision directory

--- Computer vision/save_image.py
+++
@@ -1,6 +0,0 @@
-import cv2
-img = cv2.imread(r"C:\Users\santh\OneDrive\Desktop\Computer_vision_work\img\paris.jpg", 0)
-cv2.imshow('name',img)
-cv2.imwrite('Cimg\paris_new.jpg',img)#i'm write is to save the image
-cv2.waitKey(0)
-cv2.destroyAllWindows()
